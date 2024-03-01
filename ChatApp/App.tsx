ec9acbc4ab24ce599c0e09efbd3d1eec9d7d1aec
yarzar <zar31528@gmail.com>
2024-03-01 12:54:59 +0630

    chat

--- ChatApp/App.tsx
+++ ChatApp/App.tsx
@@ -1,23 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Image, View, Text } from "react-native";
-import { useFonts } from "@expo-google-fonts/roboto";
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 const backgroundImg = require("./assets/images/background.png");
-const appIcon = require("./assets/images/app_icon.png");
+const appIcon = require("./assets/images/wtf.png");
 const welcome = require("./assets/images/welcome.png");
+const signup = require("./assets/images/signup.png");
+const signupform = require("./assets/images/signupform.png");
+const login = require("./assets/images/login.png");
 
 export default function App() {
+  const handleClick = () => {};
+
   return (
     <View style={styles.container}>
       <Image source={backgroundImg} style={styles.background} />
-      <View style={{ width: "96%", flex: 1 }}>
+      <View style={{ width: "96%" }}>
         <View style={styles.heroSection}>
           <Image source={appIcon} style={styles.appIcon} />
           <Image source={welcome} style={styles.welcome} />
           <Text style={styles.font}>
             Embark on Limitless Adventures with Our Cutting-Edge Mobile App!
           </Text>
+        </View>
+        <View style={styles.body}>
+          <View style={styles.formContainer}>
+            <Image source={signupform} style={styles.signupformstyle} />
+            <View style={styles.inputtext}>
+              <TextInput 
+              style={styles.input}
+              placeholder="Enter your email" />
+              <TextInput 
+              style={styles.input}
+              placeholder="Enter your password" />
+            </View>
+          </View>
+          <TouchableOpacity onPress={handleClick}>
+            <Image source={signup} style={styles.signupStyle} />
+          </TouchableOpacity>
+          <View style={styles.helpstyle1}>
+            <Text style={styles.font1}>already have an account? (or)</Text>
+            <TouchableOpacity onPress={handleClick}>
+              <Text style={styles.font2}> help!</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity onPress={handleClick}>
+            <Image source={login} style={styles.signinStyle} />
+          </TouchableOpacity>
         </View>
       </View>
       <StatusBar style="light" />
@@ -38,29 +74,122 @@ const styles = StyleSheet.create({
     height: "100%",
   },
   heroSection: {
+    marginTop: 10,
     width: "100%",
-    height: "50%",
-    paddingTop: 14,
+    height: "30.5%",
     alignItems: "center",
     flexDirection: "column",
   },
   appIcon: {
     width: 170,
-    height: 140,
+    height: 110,
     marginTop: 10,
   },
   welcome: {
-    marginTop: 10,
-    width: 187,
-    height: 99,
+    width: 294,
+    height: 80,
   },
   font: {
-    marginTop: 5,
     textAlign: "center",
-    color: "black",
+    color: "#853801",
     fontWeight: "900",
+    fontSize: 16.5,
+    fontStyle: "italic",
+    fontFamily: "serif",
+    textShadowOffset: { width: 1.1, height: 1.1 },
+    textShadowRadius: 2,
+    textShadowColor: "rgba(0, 0, 0, 0.3)",
+  },
+  font1: {
+    textAlign: "center",
+    color: "#853801",
+    fontSize: 10,
+    fontStyle: "italic",
+    fontFamily: "serif",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 2,
+    textShadowColor: "rgba(0, 0, 0, 0.2)",
+    marginTop: 2,
+  },
+  font2: {
+    textAlign: "center",
+    color: "blue",
+    fontWeight: "500",
+    fontSize: 11,
+    fontStyle: "italic",
+    fontFamily: "serif",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 3,
+    textShadowColor: "rgba(0, 0, 0, 0.3)",
+    marginTop: 2,
+    textDecorationLine: "underline",
+  },
+  signupStyle: {
+    width: 230,
+    height: 48,
+    resizeMode: "cover",
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  signupformstyle: {
+    width: 270,
+    height: 210,
+    marginTop: 10,
+    position: "relative",
+  },
+  body: {
+    height: "100%",
+    alignItems: "center",
+  },
+  signinStyle: {
+    width: 170,
+    height: 55,
+    resizeMode: "cover",
+    borderRadius: 20,
+  },
+  helpstyle: {
+    width: 23,
+    height: 23,
+    alignSelf: "flex-end",
+    marginLeft: 5,
+  },
+  helpstyle1: {
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "center",
+  },
+  formContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    position: "relative",
+  },
+  inputtext: {
+    color: "black",
+    position: "absolute",
+    width: 270,
+    height: 150,
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 20,
+  },
+  input: {
+    padding: 10,
+    height: 50,
+    width: 220,
+    borderWidth: 3,
+    borderColor: "#654559",
+    color: "#FF7300",
+    borderBottomRightRadius: 20,
+    borderTopLeftRadius: 20,
+    margin: 8,
+    textAlign: "center",
+    fontWeight: "800",
     fontSize: 15,
-    fontStyle: 'italic',
-    fontFamily: "sans-serif",
+    fontStyle: "italic",
+    fontFamily: "serif",
+    textShadowOffset: { width: 1, height: 1 },
+    textShadowRadius: 2,
+    textShadowColor: "rgba(0, 0, 0, 0.2)",
+    marginTop: 2,
   },
 });
--- my-project/MemberApp2/components/client.js
+++ my-project/MemberApp2/components/client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "https://1c97-103-89-180-3.ngrok-free.app/graphql" }),
+  link: new HttpLink({ uri: "https://4a36-2400-ac40-60e-4120-291f-2b2c-1017-c85f.ngrok-free.app/graphql" }),
   cache: new InMemoryCache(),
 });
 
